f2f02788c4d27839e345eb05d71e363f8900a316
SyntaxError0009 <156071408+SyntaxError0009@users.noreply.github.com>
2024-01-12 01:06:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const {Client} = require("discord.js-selfbot-v13")
+const client = new Client({
+checkUpdate: false,
+})
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Kartalm')
+})
+
+app.listen(3000)
+
+client.on("ready", async () => {
+console.log(`${client.user.username} is Ready!`)
+})
+
+const orderrr = "1188932079044542464"
+
+client.on("channelCreate", async channel => {
+if (channel.type === "GUILD_TEXT" && channel.name.startsWith("ticket-") && channel.parent.id === orderrr) {
+
+await new Promise(r => setTimeout(r, 2000))
+
+channel.send(`>  **سلاو بەرێز بەکر بەیت بۆ تیکیتک
+__MOON NIGHT__
+> داوە کاریەکەت رون کەوە** 
+ __HAMA__`)
+
+}
+});
+///
+
+client.login(process.env.token)
+
